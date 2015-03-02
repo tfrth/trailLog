93a2ba36ff476bba5aed9477a9ce81afc0cf6506
Ty Frith <tyfrith@gmail.com>
2015-03-01 22:14:52 -0700

    werk

--- public/js/controllers/mapCtrl.js
+++ public/js/controllers/mapCtrl.js
@@ -1,5 +1,12 @@
 var app = angular.module('trailApp');
 
 app.controller('mapCtrl', function($scope, mapService) {
+	
+	var mapOptions = {
+    center: new google.maps.LatLng(39.8282, -98.5795),
+    zoom: 4,
+    mapTypeId: google.maps.MapTypeId.TERRAIN
+};
 
+new google.maps.Map(document.getElementById('map'), mapOptions);
 });
